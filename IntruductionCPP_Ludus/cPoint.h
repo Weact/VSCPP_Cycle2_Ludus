48fa3582366a8642df612ee3179ddc6316a204ca
Weaact <lucas.druckes@numericable.fr>
2020-10-05 14:20:48 +0200

    Adding comment data members and methods members

--- IntruductionCPP_Ludus/cPoint.h
+++ IntruductionCPP_Ludus/cPoint.h
@@ -4,8 +4,12 @@
 
 class cPoint
 {
+    //Données membres
+
     int n_px, n_py;
 public:
+    //Fonctions membres
+
     cPoint(); //default constructor, init to NULL
     cPoint(int, int); //constructor init to values
     void set_position(int, int); //method which init the position values of a point
